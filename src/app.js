b62a516befd2a8b49090183137cb65142114e37c
mitchel webb <heyitsmemitchel@gmail.com>
2022-04-08 14:53:27 -0700

    Added full functionality to cancel button. Fixed error present with + button in previous version where text box was put in the wrong spot

--- src/app.js
+++ src/app.js
@@ -9,22 +9,22 @@ const notes = [
 //defining the areas used in this code
 const newNoteArea = document.querySelector(".create-note-area")
 const addNoteButton = document.querySelector(".fa-circle-plus")
-const textBox = `<textarea name="newnote" id="textArea" cols="30" rows="10"></textarea>
-  <button class="saveButton">save</button>
-  <button class="cancelButton">cancel</button>`
+const textBox = `<textarea name="newnote" id="textArea" cols="30" rows="10"></textarea>`
+const saveButton = `<button class="saveButton">save</button>`
+const cancelButton = `<button class="cancelButton">cancel</button>`
+
 
 // create new note button (+)
 addNoteButton.addEventListener("click", (evt) => {
-  console.log(evt);
-  newNoteArea.insertAdjacentHTML("afterend", textBox)
-
+  newNoteArea.insertAdjacentHTML("beforeend", textBox)
+  newNoteArea.insertAdjacentHTML("beforeend", saveButton)
+  newNoteArea.insertAdjacentHTML("beforeend", cancelButton)
+  //cancel current note button (cancel)
   const cancelNoteButton = document.querySelector(".cancelButton")
+  const saveNoteButton = document.querySelector(".saveButton")
   cancelNoteButton.addEventListener("click", (evt) => {
-    console.log(evt);
-    newNoteArea.removeChild(textBox);
+    textArea.remove()
+    cancelNoteButton.remove()
+    saveNoteButton.remove()
   })
 })
-
-
-
-//cancel current note button
