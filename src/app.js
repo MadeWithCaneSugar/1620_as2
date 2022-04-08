331cd039af84fe0dd078458d41f6ea68152c83c1
mitchel webb <heyitsmemitchel@gmail.com>
2022-04-07 19:42:47 -0700

    adding functionality to + button (create text box, save & cancel buttons), tried adding funcionality to cancel button(partial success)

--- src/app.js
+++ src/app.js
@@ -5,3 +5,26 @@ const notes = [
     id: 1 
   }
 ]
+
+//defining the areas used in this code
+const newNoteArea = document.querySelector(".create-note-area")
+const addNoteButton = document.querySelector(".fa-circle-plus")
+const textBox = `<textarea name="newnote" id="textArea" cols="30" rows="10"></textarea>
+  <button class="saveButton">save</button>
+  <button class="cancelButton">cancel</button>`
+
+// create new note button (+)
+addNoteButton.addEventListener("click", (evt) => {
+  console.log(evt);
+  newNoteArea.insertAdjacentHTML("afterend", textBox)
+
+  const cancelNoteButton = document.querySelector(".cancelButton")
+  cancelNoteButton.addEventListener("click", (evt) => {
+    console.log(evt);
+    newNoteArea.removeChild(textBox);
+  })
+})
+
+
+
+//cancel current note button
