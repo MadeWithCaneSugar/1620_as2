258d0f670537c0f4cba73f75a39d4725775f3e98
mitchel webb <heyitsmemitchel@gmail.com>
2022-04-08 15:37:50 -0700

    added functionality to disbale the (+) button upon clicking it and then re-enabling it upon clicking (cancel)

--- src/app.js
+++ src/app.js
@@ -14,17 +14,27 @@ const saveButton = `<button class="saveButton">save</button>`
 const cancelButton = `<button class="cancelButton">cancel</button>`
 
 
-// create new note button (+)
-addNoteButton.addEventListener("click", (evt) => {
+function noteFunction() {
+  //adding the text-box, (save), and (cancel) buttons
   newNoteArea.insertAdjacentHTML("beforeend", textBox)
   newNoteArea.insertAdjacentHTML("beforeend", saveButton)
   newNoteArea.insertAdjacentHTML("beforeend", cancelButton)
-  //cancel current note button (cancel)
+
+  //disabling the (+) button
+  addNoteButton.removeEventListener("click", noteFunction)
+
+  //functionality of the (cancel) button
   const cancelNoteButton = document.querySelector(".cancelButton")
   const saveNoteButton = document.querySelector(".saveButton")
   cancelNoteButton.addEventListener("click", (evt) => {
     textArea.remove()
     cancelNoteButton.remove()
     saveNoteButton.remove()
+    
+    //re-enabling the (+) when the (cancel) button is clicked
+    addNoteButton.addEventListener("click", noteFunction)
   })
-})
+}
+
+//adding the functionality to the (+) button
+addNoteButton.addEventListener("click", noteFunction)
